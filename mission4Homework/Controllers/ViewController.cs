3893d06770a55b6d0c04147e796fb3f4a376d49d
Colton Pearce <cpearce3@byu.edu>
2023-02-06 19:29:26 -0700

    Mission5_cpearce3

---
+++ mission4Homework/Controllers/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using mission4Homework.Models;
+
+namespace mission4Homework.Controllers
+{
+    public class ViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+       [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ mission4Homework/Models/GradeCalculatorModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission4Homework.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        [Range(0, 100,
+            ErrorMessage ="Input for assignments must be between 0-100")]
+        public float assignments { get; set; }
+
+        [Required]
+        [Range(0, 100,
+            ErrorMessage = "Input for group projects must be between 0-100")]
+        public float group { get; set; }
+
+        [Required]
+        [Range(0, 100,
+            ErrorMessage = "Input for quizzes must be between 0-100")]
+        public float quizzes { get; set; }
+
+        [Required]
+        [Range(0, 100,
+            ErrorMessage = "Input for the midterm exam must be between 0-100")]
+        public float midterm { get; set; }
+
+        [Required]
+        [Range(0, 100,
+            ErrorMessage = "Input for the final exam must be between 0-100")]
+        public float final { get; set; }
+
+        [Required]
+        [Range(0, 100,
+            ErrorMessage = "Input for intex must be between 0-100")]
+        public float intex { get; set; }
+    }
+}
